c45c3fb362329e4e6cb6a347da733d73dbc622c2
proberto666 <65254723+proberto666@users.noreply.github.com>
2021-06-03 09:51:57 -0500

    Update Constants.cs
    borrado de secret key

--- AppBlobAzure/AppBlobAzure/Settings/Constants.cs
+++ AppBlobAzure/AppBlobAzure/Settings/Constants.cs
@@ -6,8 +6,7 @@ namespace AppBlobAzure.Settings
 {
     public class Constants
     {
-        public const string StorageConnection = "DefaultEndpointsProtocol=https;AccountName=archivos11blob11hyrm;AccountKey=o/3IUxEaVVdGONJlc4n0dVxEoTj2W9zSzdswA5ySw0aDcOZPN/FodtcMNdyvji5Ia4SzjbZuphticjbn/iFq6A==;EndpointSuffix=core.windows.net";
-
+        public const string StorageConnection = "key";
 
     }
 }
